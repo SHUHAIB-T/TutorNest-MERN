a8c99f6d75dd1ad6d78c35fef0c87a00362984ae
Shuhaib T U <shuhaibtu79@gmail.com>
2024-02-06 18:05:15 +0530

    database connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app";
 import { env } from "./utils/envvalid";
+import mongoose from "mongoose";
 
 const PORT = env.PORT;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+mongoose.connect(env.MONGO_URI).then(() => {
+  console.log("Database connected succefully");
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
