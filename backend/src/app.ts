e201c377cf9fecc8f718eb2c6b017152fc6858b3
Shuhaib T U <shuhaibtu79@gmail.com>
2024-02-06 17:42:44 +0530

    error middleware added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware";
 
-import userRouter from "./routes/userRoute"
+import userRouter from "./routes/userRoute";
 
 dotenv.config();
 
@@ -21,6 +22,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsConfig));
 
-app.use("/api", userRouter)
+app.use("/api", userRouter);
+
+//error handler
+app.use("*", notFound);
+app.use(errorHandler);
 
 export default app;
