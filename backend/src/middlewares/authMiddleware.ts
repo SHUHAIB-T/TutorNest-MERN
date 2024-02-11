65db7c54d05eb5888a2b99c9b196e4d57985e5e4
Shuhaib T U <shuhaibtu79@gmail.com>
2024-02-12 03:51:04 +0530

    protect middleware updated

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,29 +1,42 @@
-import asyncHandler from "express-async-handler";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { env } from "../utils/envvalid";
 import { Request, Response, NextFunction } from "express";
-import User, { IUser } from "../model/userModel";
+import asyncHandler from "express-async-handler";
+import User from "../model/userModel";
+import { env } from "../utils/envvalid";
+import mongoose, { Document } from "mongoose";
 
-interface CustomRequest extends Request {
-  user?: IUser; 
+declare module "express" {
+  interface Request {
+    user?: Document;
+  }
 }
 
 export const protect = asyncHandler(
-  async (req: CustomRequest, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.token;
     if (token) {
       try {
         const decoded = jwt.verify(token, env.JWT_SECRET) as JwtPayload;
-        const user = (await User.findById(decoded.userId)) as IUser;
-        req.user = user;
+        const userId = new mongoose.Types.ObjectId(decoded.userId);
+        const user = await User.findOne({ _id: userId });
+        if (!user) {
+          res.status(401);
+          next(Error("Unauthorized user"));
+        } else if (!user.status) {
+          res.status(401);
+          next(Error("Account has been blocked"));
+        } else {
+          req.user = user;
+        }
         next();
       } catch (error) {
         res.status(401);
-        throw new Error("Not Authorized, invalid tocken");
+        next(new Error("Not authorized, token failed"));
       }
     } else {
       res.status(401);
-      throw new Error("Not Authorized, no tocken");
+      res.status(401);
+      next(new Error("Not authorized, token failed"));
     }
   }
 );
