893a97f0000f73c9189772329835520c2108d599
Shuhaib T U <shuhaibtu79@gmail.com>
2024-03-17 23:42:53 +0530

    style: active style added

--- frontend/src/components/StudentSideBar/StudentSideBar.tsx
+++ frontend/src/components/StudentSideBar/StudentSideBar.tsx
@@ -7,8 +7,10 @@ import SchoolIcon from "@mui/icons-material/School";
 import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function StudentSideBar() {
+  const location = useLocation();
   const [isHidden, setIsHidden] = useState("hidden");
   return (
     <div className="flex-col w-full max-w-80 min-w-72 flex items-start">
@@ -25,31 +27,41 @@ export default function StudentSideBar() {
       >
         <Link
           to={"/student/profile"}
-          className="hover:bg-[#3f344e]/80 py-1 w-full rounded px-5 flex items-center justify-start gap-2"
+          className={`hover:bg-[#3f344e]/80 ${
+            location.pathname === "/student/profile" ? "bg-[#3f344e]/80" : ""
+          }  py-1 w-full rounded px-5 flex items-center justify-start gap-2`}
         >
           <AccountCircleIcon /> MY PROFILE
         </Link>
         <Link
           to={"/student/posts"}
-          className="hover:bg-[#3f344e]/80 py-1 w-full rounded px-5 flex items-center justify-start gap-2"
+          className={`hover:bg-[#3f344e]/80 ${
+            location.pathname === "/student/posts" ? "bg-[#3f344e]/80" : ""
+          }  py-1 w-full rounded px-5 flex items-center justify-start gap-2`}
         >
           <EditCalendarIcon /> MY POSTS
         </Link>
         <Link
           to={"#"}
-          className="hover:bg-[#3f344e]/80 py-1 w-full rounded px-5 flex items-center justify-start gap-2"
+          className={`hover:bg-[#3f344e]/80 ${
+            location.pathname === "/student/courses" ? "bg-[#3f344e]/80" : ""
+          }  py-1 w-full rounded px-5 flex items-center justify-start gap-2`}
         >
           <ArticleIcon /> MY COURSES
         </Link>
         <Link
           to={"/student/my-tutors"}
-          className="hover:bg-[#3f344e]/80 py-1 w-full rounded px-5 flex items-center justify-start gap-2"
+          className={`hover:bg-[#3f344e]/80 ${
+            location.pathname === "/student/my-tutors" ? "bg-[#3f344e]/80" : ""
+          }  py-1 w-full rounded px-5 flex items-center justify-start gap-2`}
         >
           <SchoolIcon /> My TUTORS
         </Link>
         <Link
           to={"/student/requests"}
-          className="hover:bg-[#3f344e]/80 py-1 w-full rounded px-5 flex items-center justify-start gap-2"
+          className={`hover:bg-[#3f344e]/80 ${
+            location.pathname === "/student/requests" ? "bg-[#3f344e]/80" : ""
+          }  py-1 w-full rounded px-5 flex items-center justify-start gap-2`}
         >
           <ConnectWithoutContactIcon /> REQUESTS
         </Link>
